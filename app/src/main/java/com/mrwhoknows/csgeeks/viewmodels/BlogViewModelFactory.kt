d4f88517ae39190d88ba9a8a267b8ec01f914237
mrwhoknows55 <avdhuttanugade55@gmail.com>
2020-08-09 16:08:12 +0530

    added view model to author data

--- app/src/main/java/com/mrwhoknows/csgeeks/viewmodels/BlogViewModelFactory.kt
+++ app/src/main/java/com/mrwhoknows/csgeeks/viewmodels/BlogViewModelFactory.kt
@@ -4,10 +4,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.mrwhoknows.csgeeks.repository.BlogRepository
 
-class ArticleViewModelFactory(
+class BlogViewModelFactory(
     private val repository: BlogRepository
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return ArticleViewModel(repository) as T
+        return BlogViewModel(repository) as T
     }
 }
