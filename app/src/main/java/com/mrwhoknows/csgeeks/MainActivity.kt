0d0d70d3a568f67d4adda413e10b2c31cfae2a56
mrwhoknows55 <avdhuttanugade55@gmail.com>
2020-08-12 23:17:49 +0530

    added back button in toolbar

--- app/src/main/java/com/mrwhoknows/csgeeks/MainActivity.kt
+++ app/src/main/java/com/mrwhoknows/csgeeks/MainActivity.kt
@@ -3,9 +3,12 @@ package com.mrwhoknows.csgeeks
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.mrwhoknows.csgeeks.repository.BlogRepository
 import com.mrwhoknows.csgeeks.viewmodels.BlogViewModel
 import com.mrwhoknows.csgeeks.viewmodels.BlogViewModelFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 private const val TAG = "MainActivity"
 
@@ -16,6 +19,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(mainToolbar)
 
         val blogRepository = BlogRepository()
         val viewModelFactory =
@@ -23,5 +27,13 @@ class MainActivity : AppCompatActivity() {
                 blogRepository
             )
         viewModel = ViewModelProvider(this, viewModelFactory).get(BlogViewModel::class.java)
+
+        val navController = this.findNavController(R.id.navHostFragment)
+        NavigationUI.setupActionBarWithNavController(this,navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.navHostFragment)
+        return navController.navigateUp()
     }
 }
