8e27be7568bcc70c923bb70a0c6d449bd502dd07
mrwhoknows55 <avdhuttanugade55@gmail.com>
2020-08-22 14:42:27 +0530

    add category chips on home

--- app/src/main/java/com/mrwhoknows/csgeeks/ui/article/ArticlesListFragment.kt
+++ app/src/main/java/com/mrwhoknows/csgeeks/ui/article/ArticlesListFragment.kt
@@ -3,10 +3,12 @@ package com.mrwhoknows.csgeeks.ui.article
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.chip.Chip
 import com.google.android.material.snackbar.Snackbar
 import com.mrwhoknows.csgeeks.MainActivity
 import com.mrwhoknows.csgeeks.R
@@ -16,6 +18,8 @@ import com.mrwhoknows.csgeeks.util.Resource
 import com.mrwhoknows.csgeeks.util.Util
 import com.mrwhoknows.csgeeks.viewmodels.BlogViewModel
 import kotlinx.android.synthetic.main.fragment_articles_list.*
+import kotlinx.android.synthetic.main.fragment_articles_list.bounceLoader
+import kotlinx.android.synthetic.main.fragment_articles_list.bounceLoaderBG
 
 private const val TAG = "ListFragment"
 
@@ -28,6 +32,7 @@ class ArticlesListFragment : Fragment(R.layout.fragment_articles_list) {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel = (activity as MainActivity).viewModel
+        initCategories()
 
         val sharedPreferences = requireActivity().getSharedPreferences("TOKEN", 0)
         if (sharedPreferences.getBoolean("IS_LOGGED_IN", false)) {
@@ -90,5 +95,44 @@ class ArticlesListFragment : Fragment(R.layout.fragment_articles_list) {
             layoutManager = LinearLayoutManager(context)
             adapter = articleAdapter
         }
+    }
+
+    private fun initCategories() {
+        var tags: List<String>
+        viewModel.getArticleTags()
+        viewModel.tags.observe(viewLifecycleOwner, Observer { resource ->
+            when (resource) {
+                is Resource.Success -> {
+                    Log.d(TAG, "tags: ${resource.data}")
+                    resource.data?.let {
+                        tags = it.tags
+                        for (tag in tags) {
+                            val chip = Chip(chipsCategories.context)
+                            chip.text = tag
+                            chip.chipBackgroundColor = ContextCompat.getColorStateList(
+                                requireContext(),
+                                R.color.colorBackgroundDark2
+                            )
+
+                            chip.isClickable = true
+                            chip.isCheckable = true
+                            chipsCategories.addView(chip)
+                        }
+                    }
+                    chipsCategories.isSingleSelection = true
+
+                    chipsCategories.setOnCheckedChangeListener { group, id ->
+                        val chip = chipsCategories.findViewById<Chip>(id)
+                        if (chip != null) {
+                            Log.d(TAG, "chip sel: ${chip.text}")
+                            viewModel.getArticlesByTag(chip.text.toString())
+                        } else {
+                            Log.d(TAG, "chip not selected")
+                            viewModel.getAllArticles()
+                        }
+                    }
+                }
+            }
+        })
     }
 }
