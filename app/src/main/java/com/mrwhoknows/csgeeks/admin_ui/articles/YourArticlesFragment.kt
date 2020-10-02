2c5cf5fa31a3f52101b3da3ba54c354a70dee29a
mrwhoknows55 <avdhuttanugade55@gmail.com>
2020-10-02 14:45:45 +0530

    move search bar to action bar 🔝

--- app/src/main/java/com/mrwhoknows/csgeeks/admin_ui/articles/YourArticlesFragment.kt
+++ app/src/main/java/com/mrwhoknows/csgeeks/admin_ui/articles/YourArticlesFragment.kt
@@ -41,8 +41,6 @@ class YourArticlesFragment : Fragment(R.layout.fragment_articles_list) {
 
         viewModel = (activity as AdminActivity).viewModel
 
-
-        etSearchArticles.visibility = View.GONE
         spFilterBy.visibility = View.GONE
         showYourArticles()
         swipeToDeleteArticle()
