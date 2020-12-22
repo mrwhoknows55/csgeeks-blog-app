8de10c21913b933365d897aa11329b894bccc67d
Avadhut Tanugade <avdhuttanugade55@gmail.com>
2020-12-22 23:19:56 +0530

    Now user can change app night mode themes  🌚

--- app/src/main/java/com/mrwhoknows/csgeeks/ui/SplashActivity.kt
+++ app/src/main/java/com/mrwhoknows/csgeeks/ui/SplashActivity.kt
@@ -4,11 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.lifecycle.ViewModelProvider
 import com.mrwhoknows.csgeeks.R
 import com.mrwhoknows.csgeeks.repository.BlogRepository
 import com.mrwhoknows.csgeeks.ui.admin_page.AdminActivity
 import com.mrwhoknows.csgeeks.ui.home_page.MainActivity
+import com.mrwhoknows.csgeeks.util.Constants
 import com.mrwhoknows.csgeeks.util.LoginInfo
 import com.mrwhoknows.csgeeks.util.Resource
 import com.mrwhoknows.csgeeks.viewmodels.BlogViewModel
@@ -23,6 +25,13 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        val appSettingPrefs = getSharedPreferences(Constants.APP_THEME_SHARED_PREFS, MODE_PRIVATE)
+        AppCompatDelegate.setDefaultNightMode(
+            appSettingPrefs.getInt(
+                Constants.NIGHT_MODE,
+                AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+            )
+        )
 
         val loginInfo = LoginInfo(this)
 
@@ -41,8 +50,8 @@ class SplashActivity : AppCompatActivity() {
             //author or admin user
             val token = loginInfo.loginToken
 
-            if (token != null) {
-                viewModel.isLoggedUserLoggedIn(token)
+            if ((token != null) and (token != "null")) {
+                viewModel.isLoggedUserLoggedIn(token!!)
             } else {
                 navigateToActivity(main)
             }
@@ -72,6 +81,6 @@ class SplashActivity : AppCompatActivity() {
 
     private fun navigateToActivity(intent: Intent) {
         startActivity(intent)
-        finishAfterTransition()
+        finish()
     }
 }
--- app/src/main/java/com/mrwhoknows/csgeeks/ui/admin_page/AdminActivity.kt
+++ app/src/main/java/com/mrwhoknows/csgeeks/ui/admin_page/AdminActivity.kt
@@ -2,9 +2,12 @@ package com.mrwhoknows.csgeeks.ui.admin_page
 
 import android.content.DialogInterface
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.MotionEvent
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
@@ -14,8 +17,10 @@ import com.google.android.material.snackbar.Snackbar
 import com.mrwhoknows.csgeeks.R
 import com.mrwhoknows.csgeeks.repository.BlogRepository
 import com.mrwhoknows.csgeeks.ui.home_page.MainActivity
+import com.mrwhoknows.csgeeks.util.Constants
 import com.mrwhoknows.csgeeks.util.Constants.AUTHOR_NAME
 import com.mrwhoknows.csgeeks.util.Constants.LOGIN_TOKEN
+import com.mrwhoknows.csgeeks.util.Constants.NIGHT_MODE
 import com.mrwhoknows.csgeeks.util.Constants.TOKEN_SHARED_PREFF
 import com.mrwhoknows.csgeeks.util.Resource
 import com.mrwhoknows.csgeeks.util.collapseKeyboardIfFocusOutsideEditText
@@ -27,14 +32,24 @@ private const val TAG = "AdminActivity"
 
 class AdminActivity : AppCompatActivity() {
 
+    private lateinit var appSettingPrefs: SharedPreferences
+    private lateinit var adminSharedPrefs: SharedPreferences
     lateinit var viewModel: BlogViewModel
     var USER_TOKEN = ""
     var AUTHOR = ""
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin)
         setSupportActionBar(adminToolbar)
+        appSettingPrefs = getSharedPreferences(Constants.APP_THEME_SHARED_PREFS, MODE_PRIVATE)
+        AppCompatDelegate.setDefaultNightMode(
+            appSettingPrefs.getInt(
+                NIGHT_MODE,
+                AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+            )
+        )
 
         val blogRepository = BlogRepository()
         val viewModelFactory =
@@ -43,58 +58,116 @@ class AdminActivity : AppCompatActivity() {
             )
         viewModel = ViewModelProvider(this, viewModelFactory).get(BlogViewModel::class.java)
 
-        val adminSharedPrefs = this.getSharedPreferences(TOKEN_SHARED_PREFF, 0)
+        adminSharedPrefs = this.getSharedPreferences(TOKEN_SHARED_PREFF, 0)
         USER_TOKEN = adminSharedPrefs.getString(LOGIN_TOKEN, null).toString()
         AUTHOR = adminSharedPrefs.getString(AUTHOR_NAME, null).toString()
 
-        initLogoutInMenu()
+        setNavMenuItemClicks()
         val navController = findNavController(R.id.adminNavHostFragment)
         NavigationUI.setupWithNavController(adminNavView, navController)
         NavigationUI.setupActionBarWithNavController(this, navController, adminDrawerLayout)
     }
 
-    private fun initLogoutInMenu() {
+    private fun setNavMenuItemClicks() {
         adminNavView.menu.getItem(4).setOnMenuItemClickListener {
-
-            val dialogClickListener: DialogInterface.OnClickListener =
-                DialogInterface.OnClickListener { _, which ->
-                    when (which) {
-                        DialogInterface.BUTTON_POSITIVE -> {
-                            logoutUser()
-                        }
-                        DialogInterface.BUTTON_NEGATIVE -> {
-                        }
-                    }
-                }
-
-            val builder = MaterialAlertDialogBuilder(this)
-            builder.setTitle("Logout")
-                .setMessage("Are you sure?")
-                .setBackground(
-                    ContextCompat.getDrawable(
-                        baseContext,
-                        R.color.colorBackgroundDark3
-                    )
-                )
-                .setPositiveButton("Yes, Logout", dialogClickListener)
-                .setNegativeButton("No", dialogClickListener).show()
-
+            setLogoutDialog()
+            true
+        }
+        adminNavView.menu.getItem(3).setOnMenuItemClickListener {
+            setAppThemeDialog()
             true
         }
     }
 
+    private fun setAppThemeDialog() {
+        val isNightModeOn =
+            appSettingPrefs.getInt(NIGHT_MODE, AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+        val appSettingPrefsEditor = appSettingPrefs.edit()
+
+        AppCompatDelegate.setDefaultNightMode(isNightModeOn)
+        var checkedItem: Int = when (isNightModeOn) {
+            AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM, AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY -> 2
+            AppCompatDelegate.MODE_NIGHT_YES -> 0
+            AppCompatDelegate.MODE_NIGHT_NO -> 1
+            else -> 2
+        }
+        val singleItems = arrayOf("Dark", "Light", "Auto")
+
+        MaterialAlertDialogBuilder(this)
+            .setTitle("App Theme")
+            .setBackground(ContextCompat.getDrawable(baseContext, R.color.colorBackgroundDark3))
+            .setNeutralButton("Cancel") { dialog, _ ->
+                dialog.cancel()
+            }
+            .setSingleChoiceItems(singleItems, checkedItem) { dialog, which ->
+                checkedItem = which
+            }
+            .setPositiveButton("Apply") { dialog, which ->
+                when (checkedItem) {
+                    0 -> {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                        appSettingPrefsEditor.putInt(NIGHT_MODE, AppCompatDelegate.MODE_NIGHT_YES)
+                    }
+                    1 -> {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                        appSettingPrefsEditor.putInt(NIGHT_MODE, AppCompatDelegate.MODE_NIGHT_NO)
+                    }
+                    else -> {
+                        AppCompatDelegate.setDefaultNightMode(
+                            AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+                        )
+                        appSettingPrefsEditor.putInt(
+                            NIGHT_MODE,
+                            AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+                        )
+                    }
+                }
+                appSettingPrefsEditor.apply()
+                dialog.dismiss()
+            }.show()
+    }
+
+    private fun setLogoutDialog() {
+        val dialogClickListener: DialogInterface.OnClickListener =
+            DialogInterface.OnClickListener { _, which ->
+                when (which) {
+                    DialogInterface.BUTTON_POSITIVE -> {
+                        logoutUser()
+                    }
+                    DialogInterface.BUTTON_NEGATIVE -> {
+                    }
+                }
+            }
+
+        val builder = MaterialAlertDialogBuilder(this)
+        builder.setTitle("Logout")
+            .setMessage("Are you sure?")
+            .setBackground(
+                ContextCompat.getDrawable(
+                    baseContext,
+                    R.color.colorBackgroundDark3
+                )
+            )
+            .setPositiveButton("Yes, Logout", dialogClickListener)
+            .setNegativeButton("No", dialogClickListener).show()
+
+    }
+
+
     private fun logoutUser() {
+        val editor = adminSharedPrefs.edit()
         viewModel.logoutUserFromServer(USER_TOKEN)
 
         viewModel.logoutUserFromLiveData.observe(this, {
             when (it) {
                 is Resource.Success -> {
                     Snackbar.make(adminToolbar, "Log Out Success!", Snackbar.LENGTH_SHORT).show()
+                    editor.putString(LOGIN_TOKEN, null).apply()
                     val intent = Intent(this, MainActivity::class.java).apply {
                         flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
                     }
                     startActivity(intent)
-                    finishAfterTransition()
+                    finish()
                 }
                 is Resource.Error -> {
                     Snackbar.make(adminToolbar, "Something Went Wrong", Snackbar.LENGTH_SHORT)
